0676825927684327255de8d36348a2c4cf4c50fc
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2013-10-14 22:10:10 +0100

    Añadidos prototipos de los métodos de la clase Fraccion

--- racional.rb
+++ racional.rb
@@ -3,4 +3,37 @@
 require "./gcd.rb"
 
 class Fraccion
+
+  def initialize(a = 0, b = 1)
+
+  end
+
+  def normalizar
+    mcd = gcd(a, b)
+    if mcd != 0
+      @a /= mcd
+      @b /= mcd
+    end
+  end
+
+  def suma(otra)
+
+  end
+
+  def resta(otra)
+
+  end
+
+  def producto(otra)
+
+  end
+
+  def division(otra)
+
+  end
+
+  def to_s
+
+  end
+
 end
