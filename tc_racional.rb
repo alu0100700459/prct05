21e4b726440aa407f5a5bc38685d8b92081a7d45
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2013-10-15 17:30:46 +0100

    Añadido el test para la suma

--- tc_racional.rb
+++ tc_racional.rb
@@ -14,4 +14,19 @@ class Test_Fraccion < Test::Unit::TestCase
 		assert_raise (RuntimeError) { Fraccion.new(3, 0) }
 	end
 
+	def test_suma
+		f1 = Fraccion.new(2, 9)
+		f2 = Fraccion.new(3, 6) # La fracción no está simplificada
+		f3 = Fraccion.new(26, 36) # f3 = f1+f2, f3 no está simplificada
+		assert_equal(f3.a, f1.suma(f2).a)
+		assert_equal(f3.b, f1.suma(f2).b)
+
+		# Cambiamos los signos para ver si la suma es todavía correcta
+		f1.b=-9
+		f2.a=-3
+		f3.b=-36
+		assert_equal(f3.a, f1.suma(f2).a)
+		assert_equal(f3.b, f1.suma(f2).b)
+	end
+
 end
