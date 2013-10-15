04aeb5cd92b81fe3f4e95c2da29d7ea29cd50772
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2013-10-15 18:09:27 +0100

    Reparadas las indentaciones

--- tc_racional.rb
+++ tc_racional.rb
@@ -6,44 +6,44 @@ require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
 
-	def test_constructor
-		assert_equal(Fraccion.new(0, 1).a, Fraccion.new(0,5).a)
-		assert_equal(Fraccion.new(0, 1).b, Fraccion.new(0,5).b)
-		assert_equal(Fraccion.new(2, 3).a, Fraccion.new(8, 12).a)
-		assert_equal(Fraccion.new(2, 3).b, Fraccion.new(8, 12).b)
-		assert_raise (RuntimeError) { Fraccion.new(3, 0) }
-	end
+  def test_constructor
+    assert_equal(Fraccion.new(0, 1).a, Fraccion.new(0,5).a)
+    assert_equal(Fraccion.new(0, 1).b, Fraccion.new(0,5).b)
+    assert_equal(Fraccion.new(2, 3).a, Fraccion.new(8, 12).a)
+    assert_equal(Fraccion.new(2, 3).b, Fraccion.new(8, 12).b)
+    assert_raise (RuntimeError) { Fraccion.new(3, 0) }
+  end
 
-	def test_suma
-		f1 = Fraccion.new(2, 9)
-		f2 = Fraccion.new(3, 6) # La fracción no está simplificada
-		f3 = Fraccion.new(26, 36) # f3 = f1+f2, f3 no está simplificada
-		assert_equal(f3.a, f1.suma(f2).a)
-		assert_equal(f3.b, f1.suma(f2).b)
+  def test_suma
+    f1 = Fraccion.new(2, 9)
+    f2 = Fraccion.new(3, 6) # La fracción no está simplificada
+    f3 = Fraccion.new(26, 36) # f3 = f1+f2, f3 no está simplificada
+    assert_equal(f3.a, f1.suma(f2).a)
+    assert_equal(f3.b, f1.suma(f2).b)
 
-		# Cambiamos los signos para ver si la suma es todavía correcta
-		f1.b=-9
-		f2.a=-3
-		f3.a=31
-		f3.b=-18
-		assert_equal(f3.a, f1.suma(f2).a)
-		assert_equal(f3.b, f1.suma(f2).b)
-	end
+    # Cambiamos los signos para ver si la suma es todavía correcta
+    f1.b=-9
+    f2.a=-3
+    f3.a=31
+    f3.b=-18
+    assert_equal(f3.a, f1.suma(f2).a)
+    assert_equal(f3.b, f1.suma(f2).b)
+  end
 
-	def test_diferencia
-		f1 = Fraccion.new(2, 9)
-		f2 = Fraccion.new(6, 12) # La fracción no está simplificada
-		f3 = Fraccion.new(5, -18) # f3 = f1-f2
-		assert_equal(f3.a, f1.resta(f2).a)
-		assert_equal(f3.b, f1.resta(f2).b)
-	end
+  def test_diferencia
+    f1 = Fraccion.new(2, 9)
+    f2 = Fraccion.new(6, 12) # La fracción no está simplificada
+    f3 = Fraccion.new(5, -18) # f3 = f1-f2
+    assert_equal(f3.a, f1.resta(f2).a)
+    assert_equal(f3.b, f1.resta(f2).b)
+  end
 
-	def test_multiplicacion
-		f1 = Fraccion.new(2, -7)
-		f2 = Fraccion.new(-3, 12) # La fracción no está simplificada
-		f3 = Fraccion.new(1, 14) # f3 = f1*f2
-		assert_equal(f3.a, f1.producto(f2).a)
-		assert_equal(f3.b, f1.producto(f2).b)
-	end
+  def test_multiplicacion
+    f1 = Fraccion.new(2, -7)
+    f2 = Fraccion.new(-3, 12) # La fracción no está simplificada
+    f3 = Fraccion.new(1, 14) # f3 = f1*f2
+    assert_equal(f3.a, f1.producto(f2).a)
+    assert_equal(f3.b, f1.producto(f2).b)
+  end
 
 end
