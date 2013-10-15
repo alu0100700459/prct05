7ae2fec4e32ac1a5115dfccb1a1d3d9d53a532cb
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2013-10-15 17:17:52 +0100

    Añadida prueba del constructor

--- tc_racional.rb
+++ tc_racional.rb
@@ -1,8 +1,17 @@
+#encoding: utf-8
 # Implementar en este fichero las Pruebas Unitarias asociadas a la clase Fraccion
 
 require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+
+	def test_constructor
+		assert_equal(Fraccion.new(0, 1).a, Fraccion.new(0,5).a)
+		assert_equal(Fraccion.new(0, 1).b, Fraccion.new(0,5).b)
+		assert_equal(Fraccion.new(2, 3).a, Fraccion.new(8, 12).a)
+		assert_equal(Fraccion.new(2, 3).b, Fraccion.new(8, 12).b)
+		assert_raise (RuntimeError) { Fraccion.new(3, 0) }
+	end
 
 end
